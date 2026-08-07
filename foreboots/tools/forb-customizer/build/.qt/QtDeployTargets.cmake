2b8318f45f5a7654709c875364f220be4f79e1d7
bluethefoxofficial <bluethefoxoffical@gmail.com>
2026-08-07 13:15:11 +0100

    Untrack foreboots tool build artifacts

--- foreboots/tools/forb-customizer/build/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_forb-customizer_FILE /home/bluet/forest/foreboots/tools/forb-customizer/build/forb-customizer)
-set(__QT_DEPLOY_TARGET_forb-customizer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_tst_roundtrip_FILE /home/bluet/forest/foreboots/tools/forb-customizer/build/tst_roundtrip)
-set(__QT_DEPLOY_TARGET_tst_roundtrip_TYPE EXECUTABLE)
--- foreboots/tools/forb-customizer/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bluet/forest/foreboots/tools/forb-customizer
-# Build directory: /home/bluet/forest/foreboots/tools/forb-customizer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test("roundtrip" "/home/bluet/forest/foreboots/tools/forb-customizer/build/tst_roundtrip")
-set_tests_properties("roundtrip" PROPERTIES  _BACKTRACE_TRIPLES "/home/bluet/forest/foreboots/tools/forb-customizer/CMakeLists.txt;37;add_test;/home/bluet/forest/foreboots/tools/forb-customizer/CMakeLists.txt;0;")
